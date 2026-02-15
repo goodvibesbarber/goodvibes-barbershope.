b3a9924b0157abb205f12f3c8643cf5112f0c166
goodvibesbarber <sanaeelghazi007@gmail.com>
2026-02-15 07:26:24 -0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Services from './components/Services';
-import BookingForm from './components/BookingForm';
-import Footer from './components/Footer';
+<main className="flex-grow">
+        <Hero />
+        <About />
+        <Services />
+        {/* We added the ID "booking" so the Navbar and Hero buttons know where to jump */}
+        <div id="booking">
+          <BookingForm />
+        </div>
+      </main>
 
 // Declare AOS since it's loaded via CDN
 declare global {
